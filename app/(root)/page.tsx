b8a558c79ac644753d8643ec148c05e2d7727512
Safayet <hossainsafayet187@gmail.com>
2025-12-01 22:29:34 +0600

    market overview and stock heat map

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,35 @@
+import TradingViewWidgets from "@/components/TradingViewWidgets";
 import { Button } from "@/components/ui/button";
+import { MARKET_OVERVIEW_WIDGET_CONFIG } from "@/lib/constants";
 
 const Home = () => {
-  return <div className="flex min-h-screen ">Home</div>;
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+
+  return (
+    <div className="flex min-h-screen home-wrapper ">
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidgets
+            title="Market Overview"
+            scriptUrl={`${scriptUrl}market-overview.js`}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+
+        <div className="md-col-span xl:col-span-2">
+          <TradingViewWidgets
+            title="Stock Heatmap"
+            scriptUrl={`${scriptUrl}stock-heatmap.js`}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
