d2d3b9433d937dbc70aea22c013518007c9137a1
Safayet <hossainsafayet187@gmail.com>
2025-12-02 01:03:58 +0600

    sign up complete

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="auth-layout">
+      <section className="auth-left-section scrollbar-hide-default">
+        <Link href="/" className="auth-logo">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="brand logo"
+            width={140}
+            height={32}
+            className="h-8 w-auto"
+          />
+        </Link>
+
+        <div className="pb-6 lg:pb-8 flex-1">{children}</div>
+      </section>
+
+      <section className="auth-right-section">
+        <div className="z-10 relative lg:mt-4 lg:mb-16">
+          <blockquote className="auth-blockquote">
+            The stock market is a network of exchanges where investors buy and
+            sell ownership shares of publicly traded companies. It helps
+            companies raise money and allows investors to participate in
+            economic growth.
+          </blockquote>
+
+          <div className="flex items-center justify-between">
+            <div>
+              <cite className="auth-testimonial-author">
+                - Random text taken
+              </cite>
+              <p className="max-md:text-xs text-gray-500">
+                No designation required
+              </p>
+            </div>
+            <div className="flex items-center gap-0.5">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <Image
+                  src="/assets/icons/star.svg"
+                  key={star}
+                  width={20}
+                  height={20}
+                  className="w-5 h-5"
+                  alt="star"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 relative">
+          <Image
+            src="/assets/images/dashboard.png"
+            alt="Dashboard"
+            height={1440}
+            width={1150}
+            className="auth-dashboard-preview absolute top-0"
+          />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default layout;
