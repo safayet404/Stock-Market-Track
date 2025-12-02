bb72e6d60b64a55af60eb8a4ea665ec5a215f539
Safayet <hossainsafayet187@gmail.com>
2025-12-02 19:37:15 +0600

    inngest setup

---
+++ lib/inngest/functions.ts
@@ -0,0 +1,49 @@
+import { success } from "better-auth";
+import { inngest } from "./client";
+import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts";
+
+export const sendSignUpEmail = inngest.createFunction(
+  { id: "sign-up-email" },
+  { event: "app/user.created" },
+  async ({ event, step }) => {
+    const useProfile = `
+        - Country : ${event.data.country}
+        - Investment goals : ${event.data.investmentGoals}
+        - Risk tolerance : ${event.data.riskTolerance}
+        - Preferred Industry : ${event.data.preferredIndustry}
+        `;
+
+    const prompt = PERSONALIZED_WELCOME_EMAIL_PROMPT.replace(
+      "{{userProfile}}",
+      useProfile
+    );
+
+    const response = await step.ai.infer("generate-welcome-intro", {
+      model: step.ai.models.gemini({ model: "gemini-2.5-flash-lite" }),
+      body: {
+        contents: [
+          {
+            role: "user",
+            parts: [
+              {
+                text: prompt,
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    await step.run("send-welcome-email", async () => {
+      const part = response.candidates?.[0]?.content?.parts?.[0];
+      const introText =
+        (part && "text" in part ? part.text : null) ||
+        "Thanks for joinig us.You have the tools to track stock market";
+    });
+
+    return {
+      success: true,
+      message: "Welcome email sent seccuessfully",
+    };
+  }
+);
