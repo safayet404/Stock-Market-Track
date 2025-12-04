462b05dc739cc80413fb3a235be7bfda50d320dd
Safayet <hossainsafayet187@gmail.com>
2025-12-04 17:22:02 +0600

    send daily news letter

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,30 +1,33 @@
-'user server'
+"user server";
 
 import { connectToDatabase } from "@/DATABASE/mongoose";
 import { use } from "react";
 
 export const getAllUsersForNewsEmail = async () => {
-    try {
-        const mongoose = await connectToDatabase()
+  try {
+    const mongoose = await connectToDatabase();
 
-        const db = mongoose.connection.db;
+    const db = mongoose.connection.db;
 
-        if (!db) throw new Error('Mongoose connection not connected')
-        
-        const users = await db.collection('user').find(
-            { email: { $exists: true, $ne: null } },
-            {projection: {_id:1,id:1,email:1,name:1,country:1}}
-        ).toArray()
+    if (!db) throw new Error("Mongoose connection not connected");
 
-        return users.filter((user) => user.email && user.name).map((user) => ({
-            user._id?.toString() || '',
-            email: user.email,
-            name:user.name
-        }))
+    const users = await db
+      .collection("user")
+      .find(
+        { email: { $exists: true, $ne: null } },
+        { projection: { _id: 1, id: 1, email: 1, name: 1, country: 1 } }
+      )
+      .toArray();
 
-    } catch (error) {
-        console.error('Error in feching users', error);
-        return []
-        
-    }
-}
+    return users
+      .filter((user) => user.email && user.name)
+      .map((user) => ({
+        id: user.id || user._id?.toString() || "",
+        email: user.email,
+        name: user.name,
+      }));
+  } catch (error) {
+    console.error("Error in feching users", error);
+    return [];
+  }
+};
--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
 import { WELCOME_EMAIL_TEMPLATE } from "./template";
+import { NEWS_SUMMARY_EMAIL_PROMPT } from "../inngest/prompts";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -24,6 +25,31 @@ export const sendWelcomeEmail = async ({
     to: email,
     subject: `Welcome to Stock market tracker`,
     text: "Thanks for joining us",
+    html: htmlTemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export const sendNewsSummaryEmail = async ({
+  email,
+  date,
+  newsContent,
+}: {
+  email: string;
+  date: string;
+  newsContent: string;
+}): Promise<void> => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_PROMPT.replace(
+    "{{date}}",
+    date
+  ).replace("{{newsContent}}", newsContent);
+
+  const mailOptions = {
+    from: `"Signalist News" <signalist@jsmastery.pro>`,
+    to: email,
+    subject: `📈 Market News Summary Today - ${date}`,
+    text: `Today's market news summary from Signalist`,
     html: htmlTemplate,
   };
 
