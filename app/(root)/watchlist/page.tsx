99a01d6770776cb9dc89cd52dab80be8f38e1239
Safayet <hossainsafayet187@gmail.com>
2025-12-06 23:14:37 +0600

    wishlist

---
+++ app/(root)/watchlist/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="flex items-center text-5xl justify-center h-screen font-bold">
+      Yet to develop
+    </div>
+  );
+};
+
+export default page;
